e65d18c2123c410f74d711229975b5567d4aaf70
makombekepha <makombekepha@gmail.com>
2021-05-11 18:18:47 +0300

    subsequent commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [word, setWord]=useState("")
+  const [dialect, setDialect]=useState("")
+  const [meaning, setMeaning]=useState("")
+  const [tlang, setTlang]=useState("")
+
+  const postData=async()=>{
+    await Axios.post("https://abasuba.herokuapp.com/words",{
+      word:word,
+      dialect:dialect,
+      meaning:meaning,
+      tlang:tlang
+    })
+    setMeaning("");
+    setDialect("");
+    setTlang("");
+    setWord("");
+  }
+  const handleSubmit=(e)=>{
+    
+    e.preventDefault()
+    /*Axios.post("http://localhost:4000/words",{
+      word:word,
+      dialect:dialect,
+      meaning:meaning,
+      tlang:tlang
+    }).then(()=>{
+      alert("Success") 
+    })*/
+    postData()
+    alert("Submitted")
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="intro">
+  <h1>Welcome</h1>
+  <p>Please help us identify and define various Basuba words</p>
+</div>
+     <form className="form" onSubmit={handleSubmit}>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Suba Word</label>
+    <input type="text" 
+    onChange={e=>e.target.value}
+    placeholder="Enter the Basuba Word"
+    className="form-control" id="word" aria-describedby="emailHelp"/>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Dialect</label>
+    <input type="text"
+    onChange={e=>e.target.value}
+    placeholder="Olusuba or Olusuva"
+    className="form-control" id="dialect"/>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputEmail1" className="form-label">Meaning</label>
+    <input type="text"
+    onChange={e=>e.target.value} 
+    placeholder="Enter the meaning of the basuba word"
+    className="form-control" id="meaning" aria-describedby="emailHelp"/>
+  </div>
+  <div className="mb-3">
+    <label htmlFor="exampleInputPassword1" className="form-label">Translation Language</label>
+    <input type="text" 
+    onChange={e=>e.target.value}
+    placeholder="Enter the translation language"
+    className="form-control" id="tlang"/>
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
     </div>
   );
 }
